d54fd64b4a32272a6492aaf9a0d29a7f71a261ef
lcpowers <lipo6619@colorado.edu>
2021-04-05 06:20:16 -0600

    Create super simple models

--- R/NEONscripts/downloadPhenoCam.R
+++ R/NEONscripts/downloadPhenoCam.R
@@ -3,6 +3,7 @@
 ##' @param URL  web address where data is archived
 ##' @param skipNum The number of lines to skip (22 for 1day file and 17 for roistats file)
 ##' @export
+
 download.phenocam <- function(URL,skipNum=22) {
   ## check that we've been passed a URL
   if (length(URL) == 1 & is.character(URL) & substr(URL,1,4)=="http") {
