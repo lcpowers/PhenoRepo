d4d06e5412fdf513c3f9d0160cab69127f7ffdc7
caseymid48 <casey.middleton@colorado.edu>
2021-04-16 11:51:56 -0600

    Got model running with constant params

--- R/model_sim.R
+++ R/model_sim.R
@@ -2,10 +2,24 @@
 
 library(odin)
 library(devtools)
+library(dde)
 path_pheno_model <- "R/PhenoModel.R"
 
-path_sir_model <- system.file("examples/discrete_deterministic_sir.R", package = "odin")
 #######    Compile Model using odin    -------------------------------------
 
 model_generator <- odin::odin(path_pheno_model)
-model_generator
+model <-model_generator() # generate an instance of model
+
+#######   Run Model   -------------------------------------------------------
+
+phen_col = c("#036722","#350c5e")
+runs = 10
+
+sims <- model$run(1:runs)
+
+matplot(sims[,1], sims[,-1], xlab = "Time", ylab = "Proportion of pixels",
+        type = "l", col = phen_col, lty = 1)
+legend("topright", lwd = 1, col = phen_col, legend = c("Green", "Non-Green"), bty = "n")
+
+
+
