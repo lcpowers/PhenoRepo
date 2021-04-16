b175751a179f35b7de990ba8c6c24f79348beb2c
caseymid48 <casey.middleton@colorado.edu>
2021-04-16 12:28:14 -0600

    Vars as vectors

--- R/PhenoModel.R
+++ R/PhenoModel.R
@@ -1,18 +1,24 @@
 # Phenology Mechanistic Model Script
 
 ## Core Equations 
-update(G) <- (1 - delta) * G + beta * RB
-update(RB) <- (1 - beta) * RB + delta * G
+update(G[]) <- (1 - delta) * G[i] + beta[i] * RB[i]
+update(RB[]) <- (1 - beta[i]) * RB[i] + delta * G[i]
 
 # Total pixels should add up to one
-N <- G + RB
+#N[] <- G[i] + RB[i]
+
+# Assign dimensions
+dim(G) = 365
+dim(RB) = 365
+#dim(N) = 365
+dim(beta) = 365
 
 # Initial Conditions
-initial(G) <- G_init  # user-defined
-initial(RB) <- 1 - G_init
+initial(G[]) <- G_init  # user-defined
+initial(RB[]) <- 1 - G_init
 
 # Parameters
 G_init <- 0.2
-beta <- if (i<4) 3 else 0
+beta[] <- .05
 delta <- .1
 
