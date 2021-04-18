cd8329cd8aefd0a751892582fa09fcb8302b6777
caseymid48 <casey.middleton@colorado.edu>
2021-04-18 09:04:28 -0600

    Updated pheno model code with help from Brett

--- R/PhenoModel.R
+++ R/PhenoModel.R
@@ -1,24 +1,52 @@
-# Phenology Mechanistic Model Script
+# Phenology Model - NEON Challenge
 
-## Core Equations 
-update(G[]) <- (1 - delta) * G[i] + beta[i] * RB[i]
-update(RB[]) <- (1 - beta[i]) * RB[i] + delta * G[i]
+PhenoModel <- function(x,G_init,a,b,c) {
+  
+  n <- length(x)
+  
+  # Parameters
+  beta <- a * (x > 100 & x <= 130)
+  
+  delta <- b * (x > 130 & x <= 290) +
+    c * (x > 290 & x <= 310)
+  
+  # Create Vectors
+  G <- rep(0,n)
+  N <- rep(0,n)
+  
+  # Initial conditions
+  G[1] = G_init
+  N[1] = 1 - G_init
+  
+  # Model sim
+  for ( i in 2:n ) {
+    
+    G[i] = (1 - delta[i]) * G[i-1] + beta[i] * N[i-1]
+    N[i] = (1 - beta[i]) * N[i-1] + delta[i] * G[i-1]
+    
+  }
+  return(G)
+  
+}
 
-# Total pixels should add up to one
-#N[] <- G[i] + RB[i]
 
-# Assign dimensions
-dim(G) = 365
-dim(RB) = 365
-#dim(N) = 365
-dim(beta) = 365
+# Run model
 
-# Initial Conditions
-initial(G[]) <- G_init  # user-defined
-initial(RB[]) <- 1 - G_init
-
-# Parameters
+x <- 1:364
 G_init <- 0.2
-beta[] <- .05
-delta <- .1
+a <- 0.1
+b <- 0.1
+c <- 0.1
+
+out <- PhenoModel(x,G_init,a,b,c)
+
+plot(x,out$G,ylim=c(0,1),type="l",col="green")
+
+# Add some observation error
+# Suggest logit scale with Normal distribution for error
+sd_obs <- 0.4
+simdat <- rnorm(length(out$G),log(out$G/(1-out$G)),sd_obs)
+simdat <- exp(simdat) / (1 + exp(simdat))
+
+points(x,simdat,col="green")
 
