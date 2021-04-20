c0120a74bed2b2d87a1198439a2406ef1012fb5a
lcpowers <lipo6619@colorado.edu>
2021-04-19 19:04:28 -0600

    Revise approach to getting temp values for NOAA data

--- R/GetNOAAForecastData.R
+++ R/GetNOAAForecastData.R
@@ -11,9 +11,9 @@ library(neon4cast)
 source("R/NEONscripts/noaa_gefs_read.R")
 
 site_list <- list("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ")
-date <- "2021-04-15"
+date <- Sys.Date()
 interval = "6hr"
-cycle <- "12"
+cycle <- "18"
 # Get current NOAA forecast data
 lapply(site_list, download_noaa, cycle=cycle, dir = "data/drivers/",date=date,interval=interval)
 
@@ -21,15 +21,34 @@ lapply(site_list, download_noaa, cycle=cycle, dir = "data/drivers/",date=date,in
 base_dir <- "data/drivers/noaa/noaa/NOAAGEFS_6hr"
 
 # DF here is the NOAA forecast data
-noaa_raw_df <- noaa_gefs_read(base_dir, date, cycle, unlist(site_list))
+noaa_raw_df <- noaa_gefs_read(base_dir, date, cycle="06", unlist(site_list))
 write_csv(noaa_raw_df,paste0("data/drivers/noaa/noaa_raw_",as.character(date),".csv"))
 
 noaa_df <- separate(data = noaa_raw_df,col = time,into = c("date","time"),sep = " ") %>% 
-  filter(time=="12:00:00") %>%
-  mutate(air_temp_C = as.numeric(air_temperature-273.15)) %>% 
-  group_by(siteID,date) %>% 
-  summarise(temp = mean(air_temp_C,na.rm=T),
-            temp_sd = sd(air_temp_C,na.rm=T),
-            temp_var = var(air_temp_C,na.rm=T))
+  mutate(air_temp_C = as.numeric(air_temperature-273.15)) 
 
-write_csv(noaa_df,paste0("data/drivers/noaa/noaa_temp_4cast_",as.character(date),".csv"))
+daily_temps <- noaa_df %>%
+  group_by(siteID,date,time) %>% 
+  summarise(ens_mean = mean(air_temp_C,na.rm=T),
+            temp_var = var(air_temp_C,na.rm=T),
+            temp_sd = sd(air_temp_C,na.rm=T)) %>% 
+  ungroup() %>% 
+  group_by(siteID,date) %>% 
+  summarise(n_obs = n(),
+            daily_max = max(ens_mean,na.rm=T),
+            daily_min = min(ens_mean,na.rm=T),
+            daily_mean = mean(ens_mean,na.rm=T),
+            daily_var = max(temp_var),
+            daily_sd = max(temp_sd))
+  
+midday_temps <- noaa_df %>% 
+  filter(time=="12:00:00") %>% 
+  group_by(siteID,date) %>% 
+  summarise(midday_mean=mean(air_temp_C,na.rm=T),
+            midday_var=var(air_temp_C,na.rm=T),
+            midday_sd=sd(air_temp_C,na.rm=T))
+setdiff(unique(daily_temps$date),unique(midday_temps$date))
+
+all_data <- merge(daily_temps,midday_temps,all=T)
+
+write_csv(all_data,paste0("data/drivers/noaa/noaa_temp_4cast_",as.character(date),".csv"))
