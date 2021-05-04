43925772402e5340c5b0b5b42eaf06300e7b013a
lcpowers <lipo6619@colorado.edu>
2021-05-04 17:03:16 -0600

    Minor changes

--- R/GetNEONwxdata.R
+++ R/GetNEONwxdata.R
@@ -138,5 +138,3 @@ ggplot(all_temp,aes(x=date,y=daily_mean))+
   geom_point(aes(color=siteID))+
   facet_wrap(~siteID)
 
-# Precip
-
--- R/calcGDD.R
+++ R/calcGDD.R
@@ -94,40 +94,7 @@ write_csv(gdd_df,"data/drivers/neon/GDDandtemp_allsites.csv")
 targets_df <- read_csv("phenology-targets.csv.gz")
 all_df <- merge(gdd_df,targets_df ,all.x=T, by.x = c("date","siteID"), by.y = c("time","siteID"))
 
-### GDD plots
-ggplot(subset(all_df,siteID=="GRSM"&year==2019),aes(x=date,y=daily_mean))+
-  geom_point(color="springgreen4",size=2,alpha=0.75)+
-  geom_hline(yintercept = base_temp,color='grey70',linetype=2)+
-  scale_x_date(date_breaks='3 months',date_labels ="%b")+
-  theme_classic(base_size = 14)+
-  labs(x="2019",y=expression('24-hr mean temperature ('*degree*C*')'))
-ggsave("PrezFigures/GRSM2019temps.png",dpi=400,height=3,width=4)
 
-ggplot(subset(all_df,siteID=="GRSM"&year==2019),aes(x=date,y=GDDdaily))+
-  geom_point(color="springgreen4",size=2,alpha=0.75)+
-  #geom_hline(yintercept = base_temp,color='grey70',linetype=2)+
-  scale_x_date(date_breaks='3 months',date_labels ="%b")+
-  theme_classic(base_size = 14)+
-  labs(x="2019",y="Daily growing degree units")
-ggsave("PrezFigures/GRSM2019GDDdaily.png",dpi=400,height=3,width=4)
-
-ggplot(subset(all_df,siteID=="GRSM"&year==2019),aes(x=date,y=GDDtotal))+
-  geom_line(color="springgreen4",size=1)+
-  #geom_hline(yintercept = base_temp,color='grey70',linetype=2)+
-  scale_x_date(date_breaks='3 months',date_labels ="%b")+
-  theme_classic(base_size = 14)+
-  labs(x="2019",y="Total growing degree units")
-ggsave("PrezFigures/GRSM2019GDDtotal.png",dpi=400,height=3,width=4)
-
-ggplot(subset(all_df,siteID=="GRSM"),aes(x=date,y=GDDtotal))+
-  geom_line(color="springgreen4",size=1)+
-  #geom_hline(yintercept = base_temp,color='grey70',linetype=2)+
-  scale_x_date(date_breaks='years',date_labels ="%b %y")+
-  theme_classic(base_size = 14)+
-  labs(x="",y="Total growing degree units")
-ggsave("PrezFigures/GRSMGDDtotal.png",dpi=400,height=3,width=4)
-
-# Start with one site "GRSM"
 
 # df_len <- length(sites)*length(base_temps)
 
